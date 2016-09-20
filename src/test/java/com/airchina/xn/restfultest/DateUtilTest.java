311c3f315e401aaa2e92a2d0bf8feeb9cc418b15
auguschen <tianhao.chen@gmail.com>
2016-09-20 16:56:19 +0800

    test date util

---
+++ src/test/java/com/airchina/xn/restfultest/DateUtilTest.java
@@ -0,0 +1,19 @@
+package com.airchina.xn.restfultest;
+
+import com.airchina.xn.utils.DateUtil;
+
+public class DateUtilTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(DateUtil.getDate());
+		System.out.println(DateUtil.getDateTime());
+		System.out.println(DateUtil.getDay());
+		System.out.println(DateUtil.getEndDate());
+
+	}
+
+	
+	
+}
